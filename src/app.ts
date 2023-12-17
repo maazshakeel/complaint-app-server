f3eebf5b5f6ac1e3c70732ec3d89157d0bb45449
maazshakeel <maaz.shakeel.bodla@gmail.com>
2023-12-18 01:44:08 +0700

    test: test to deploy

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,10 @@ const app = express();
 
 const port = config.get<string>("port");
 
+app.get("/healthcheck", async (req, res) => {
+  res.json({ status: "ok" });
+});
+
 app.listen(port, () => {
   console.log(`App started at http://localhost:${port}`);
 });
